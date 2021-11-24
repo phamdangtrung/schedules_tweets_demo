bacdeb92daf9f8ab57da8d4ee4a49afdc264aa73
Pham Dang Trung <dnhoangson.danang@gmail.com>
2021-11-24 14:29:30 +0700

    User login

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'home#index'
   get 'about-us', to: 'about#index', as: :about
+
+  # Register routes
   get 'register', to: 'registration#new'
   post 'register', to: 'registration#create'
+
+  # Login routes
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+
+  # Logout routes
+  delete 'logout', to: 'sessions#destroy'
 end
