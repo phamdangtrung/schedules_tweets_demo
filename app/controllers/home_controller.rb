0f7a31a87e70e91e743d35aca5deb57ed5411c1b
Pham Dang Trung <dnhoangson.danang@gmail.com>
2021-11-24 13:40:50 +0700

    Account Registration and Logging in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,10 @@
 
 class HomeController < ApplicationController
   def index
-    flash.now[:notice] = 'Logged in successfully!'
-    flash.now[:alert] = 'Invalid email or password!'
+    if session[:user_id]
+      @user = User.find_by(id: session[:user_id]) # find the user
+    end
+    # flash.now[:notice] = 'Logged in successfully!'
+    # flash.now[:alert] = 'Invalid email or password!'
   end
 end
