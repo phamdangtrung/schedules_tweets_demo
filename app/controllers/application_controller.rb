22af823ad641dafebfb6e4c9c17d5383834dafd0
Pham Dang Trung <dnhoangson.danang@gmail.com>
2021-11-24 16:21:02 +0700

    Added Current instance for each logged in user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :set_current_user
+
+  def set_current_user
+    Current.user = User.find(session[:user_id]) if session[:user_id]
+  end
 end
