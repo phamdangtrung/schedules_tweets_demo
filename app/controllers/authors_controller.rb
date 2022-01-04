d99a038c107fe31365884b426bc19687ff46e766
Pham Dang Trung <dnhoangson.danang@gmail.com>
2022-01-04 17:13:12 +0700

    Get author post counts

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,5 +1,19 @@
 class AuthorsController < ApplicationController
   def index
     @authors = User.where.not(id: Current.user.id).order(id: :desc)
+    @list_of_post_counts = author_post_count(@authors)
   end
+
+  private
+
+    def author_post_count(authors)
+      hash = {}
+      authors.each do |author|
+        key = author.id
+        value = Post.where(user_id: author.id).count
+        hash[key] = value
+      end
+
+      hash
+    end
 end
